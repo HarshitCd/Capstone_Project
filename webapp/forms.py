d2221c995947b6832508f07b2622497fb9e75398
Harshit Chelladurai <harshitcd@gmail.com>
2022-03-27 22:11:43 +0530

    Basic Template

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,7 +1,9 @@
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class InputForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    picture = FileField('Input Image', validators=[DataRequired(), FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Submit')
